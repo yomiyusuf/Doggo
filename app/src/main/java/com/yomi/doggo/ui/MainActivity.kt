9c2a75168a5963ad975f1a553569ffaa649f58a8
yomiyusuf <ylinkz@gmail.com>
2020-07-22 02:26:26 +1000

    add splash screen

--- app/src/main/java/com/yomi/doggo/ui/MainActivity.kt
+++ app/src/main/java/com/yomi/doggo/ui/MainActivity.kt
@@ -16,6 +16,7 @@ class MainActivity : AppCompatActivity() {
     private var snackBar: Snackbar? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
